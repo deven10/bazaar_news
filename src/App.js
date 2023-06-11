c01c2ed490f84fbacf97275f62108d0d5de013d1
Deven Umrania <umraniadeven10@gmail.com>
2023-06-11 13:20:20 +0530

    update: Designed Landing Page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Routes, Route } from "react-router-dom";
 // pages
 import { Login } from "./pages/login/Login.js";
 import { Register } from "./pages/Register/Register";
+import { LandingPage } from "./pages/Landing Page/LandingPage";
 
 import "./styling/common/Common.css";
+import "animate.css";
 
 function App() {
   return (
     <div className="main-app">
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
---
+++ src/pages/Landing Page/LandingPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./LandingPage.css";
+
+import landingPageImg from "../../images/landing-image.jpg";
+
+export const LandingPage = () => {
+  return (
+    <div className="full-page d-flex jc-center ai-center">
+      <div className="half content">
+        <h2 className="animate__animated animate__fadeInDown">
+          Welcome to Bazaar New's
+        </h2>
+        <div className="animate__animated animate__fadeInLeft">
+          <p className="p1">Searching for something?</p>
+          <p className="p2">
+            We got you covered, we've got ample of news, about sports, about
+            politics, about climate, and much more.
+          </p>
+        </div>
+        <div className="animate__animated animate__fadeInUp links">
+          <Link className="join-now">Join Now</Link>
+          <Link className="already-a-user">Already a user?</Link>
+        </div>
+      </div>
+      <div className="half landing-image">
+        <img
+          className="landing-page-image"
+          src={landingPageImg}
+          alt="landing Page"
+        />
+      </div>
+    </div>
+  );
+};
